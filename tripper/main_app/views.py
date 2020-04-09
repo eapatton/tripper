e781c0baa32f4a1431c658739bed6fe89c756fcf
eapatton <eapatton91@gmail.com>
2020-04-09 15:46:28 -0700

    first commit, hello tripper

---
+++ tripper/main_app/views.py
@@ -0,0 +1,100 @@
+from django.shortcuts import render
+
+# Create your views here.
+# Add the following import
+from django.http import HttpResponse
+
+# Define the home view
+def home(request):
+  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+
+
+  ##----------------- MELODY AREA ------------------ (line 12)###
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  ##------------------------ MERT AREA -------------------- (LINE 60)##
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  ## -------------------------- LIZ AREA ------------------ (LINE 100)##
